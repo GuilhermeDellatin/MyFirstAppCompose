caa4aee1df17f4ab7a7f3113facfa1034091105b
GuilhermeDellatin <guidellatin@gmail.com>
2021-11-20 15:40:33 -0300

    Composable Text Element

--- app/src/main/java/com/gfdellatin/myfirstappcompose/MainActivity.kt
+++ app/src/main/java/com/gfdellatin/myfirstappcompose/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.gfdellatin.myfirstappcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.gfdellatin.myfirstappcompose.ui.theme.LatoFamily
 import com.gfdellatin.myfirstappcompose.ui.theme.MyFirstAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +30,7 @@ class MainActivity : ComponentActivity() {
             MyFirstAppComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -25,14 +38,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CustomText(@StringRes textResId: Int) {
+    Text(
+        text = stringResource(id = textResId),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clickable {
+                Log.d("Welcome", "Clicked")
+            },
+        color = Color.Green,
+        //fontSize = 18.sp,
+        //fontWeight = FontWeight.Bold,
+        //fontFamily = LatoFamily,
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.body1
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyFirstAppComposeTheme {
-        Greeting("Android")
+        CustomText(textResId = R.string.app_name)
     }
 }
--- app/src/main/java/com/gfdellatin/myfirstappcompose/ui/theme/Type.kt
+++ app/src/main/java/com/gfdellatin/myfirstappcompose/ui/theme/Type.kt
@@ -2,15 +2,24 @@ package com.gfdellatin.myfirstappcompose.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.gfdellatin.myfirstappcompose.R
 
 // Set of Material typography styles to start with
+val LatoFamily = FontFamily(
+    Font(R.font.lato, FontWeight.Normal),
+    Font(R.font.lato_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = LatoFamily,
+        fontWeight = FontWeight.Bold,
+        //fontFamily = FontFamily.Default,
+        //fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
     /* Other default text styles to override
