05fb19a1573c9f1947521a2db5b6ebe700af6517
GuilhermeDellatin <guidellatin@gmail.com>
2021-11-24 23:09:26 -0300

    Extract texts to stringResource

--- app/src/main/java/com/gfdellatin/myfirstappcompose/MainActivity.kt
+++ app/src/main/java/com/gfdellatin/myfirstappcompose/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.gfdellatin.myfirstappcompose
 
 import android.os.Bundle
-import android.text.style.BackgroundColorSpan
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,12 +15,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.gfdellatin.myfirstappcompose.ui.theme.BackgroundJetPackLogoColor
-import com.gfdellatin.myfirstappcompose.ui.theme.LatoFamily
 import com.gfdellatin.myfirstappcompose.ui.theme.MyFirstAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,10 +37,10 @@ fun FeedItem() {
         Box {
             Image(
                 painter = painterResource(id = R.drawable.android_jetpack_header),
-                contentDescription = "Background Image"
+                contentDescription = stringResource(id = R.string.content_background)
             )
             Text(
-                text = "26:15",
+                text = stringResource(id = R.string.video_total_time),
                 fontSize = 12.sp,
                 color = Color.White,
                modifier = Modifier
@@ -62,28 +53,28 @@ fun FeedItem() {
             modifier = Modifier.padding(16.dp)) {
             Image(
                 painter = painterResource(id = R.drawable.jetpack_compose_icon),
-                contentDescription = "Android Image",
+                contentDescription = stringResource(id = R.string.content_logo),
                 modifier = Modifier
                     .clip(CircleShape)
                     .size(40.dp),
 
             )
             Column {
-                Text(text = "Jetpack Compose - Fundamentos")
+                Text(text = stringResource(id = R.string.jetpack_fundamentals))
                 Spacer(modifier = Modifier.size(8.dp))
                 Row {
                     Text(
-                        text = "Jetpack Compose",
+                        text = stringResource(id = R.string.jetpack_university),
                         fontSize = 12.sp,
                         fontWeight = FontWeight.Light
                     )
                     Text(
-                        text = " 247 views ",
+                        text = stringResource(id = R.string.views),
                         fontSize = 12.sp,
                         fontWeight = FontWeight.Light
                     )
                     Text(
-                        text = " - two weeks ago ",
+                        text = stringResource(id = R.string.time_video_updload),
                         fontSize = 12.sp,
                         fontWeight = FontWeight.Light
                     )
@@ -92,7 +83,7 @@ fun FeedItem() {
         }
     }
 }
-
+/*
 @Composable
 fun CustomText(@StringRes textResId: Int) {
     Text(
@@ -110,7 +101,7 @@ fun CustomText(@StringRes textResId: Int) {
         textAlign = TextAlign.Center,
         style = MaterialTheme.typography.body1
     )
-}
+}*/
 
 @Preview(showBackground = true)
 @Composable
