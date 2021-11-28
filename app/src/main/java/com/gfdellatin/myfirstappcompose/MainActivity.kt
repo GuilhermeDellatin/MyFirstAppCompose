8f4450e2ab707707dd25f6f703198b7c3384b735
GuilhermeDellatin <guidellatin@gmail.com>
2021-11-28 17:47:21 -0300

    Adding background color in column and rounded borders, and change text color

--- app/src/main/java/com/gfdellatin/myfirstappcompose/MainActivity.kt
+++ app/src/main/java/com/gfdellatin/myfirstappcompose/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,7 +35,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun FeedItem() {
-    Column {
+
+    Column(
+        modifier = Modifier
+            .background(Color.DarkGray, RoundedCornerShape(16.dp))
+            .clip(RoundedCornerShape(16.dp))
+    ) {
         Box {
             Image(
                 painter = painterResource(id = R.drawable.android_jetpack_header),
@@ -43,14 +50,16 @@ fun FeedItem() {
                 text = stringResource(id = R.string.video_total_time),
                 fontSize = 12.sp,
                 color = Color.White,
-               modifier = Modifier
-                   .align(Alignment.BottomEnd)
-                   .padding(8.dp, 8.dp)
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(8.dp, 8.dp)
             )
         }
 
-        Row(horizontalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier.padding(16.dp)) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.padding(16.dp)
+        ) {
             Image(
                 painter = painterResource(id = R.drawable.jetpack_compose_icon),
                 contentDescription = stringResource(id = R.string.content_logo),
@@ -58,30 +67,34 @@ fun FeedItem() {
                     .clip(CircleShape)
                     .size(40.dp),
 
-            )
+                )
             Column {
-                Text(text = stringResource(id = R.string.jetpack_fundamentals))
+                Text(text = stringResource(id = R.string.jetpack_fundamentals), color = Color.White)
                 Spacer(modifier = Modifier.size(8.dp))
                 Row {
                     Text(
                         text = stringResource(id = R.string.jetpack_university),
                         fontSize = 12.sp,
+                        color = Color.White,
                         fontWeight = FontWeight.Light
                     )
                     Text(
                         text = stringResource(id = R.string.views),
                         fontSize = 12.sp,
+                        color = Color.White,
                         fontWeight = FontWeight.Light
                     )
                     Text(
                         text = stringResource(id = R.string.time_video_updload),
                         fontSize = 12.sp,
+                        color = Color.White,
                         fontWeight = FontWeight.Light
                     )
                 }
             }
         }
     }
+
 }
 /*
 @Composable
