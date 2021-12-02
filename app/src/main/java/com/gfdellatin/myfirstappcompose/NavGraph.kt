b8757e4b7ab690c6ecfded3925cf28e820a18344
GuilhermeDellatin <guidellatin@gmail.com>
2021-12-01 21:00:27 -0300

    Create NavGraph.kt for Navigation in screens

---
+++ app/src/main/java/com/gfdellatin/myfirstappcompose/NavGraph.kt
@@ -0,0 +1,30 @@
+package com.gfdellatin.myfirstappcompose
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@ExperimentalAnimationApi
+@Composable
+fun SetupNavGraph(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = NavControllerScreen.Intro.route
+    ) {
+        composable(
+            route = NavControllerScreen.Intro.route
+        ) {
+            IntroScreen(navController = navController)
+        }
+        composable(
+            route = NavControllerScreen.PlayList.route
+        ) {
+            FeedItem()
+        }
+
+    }
+}
