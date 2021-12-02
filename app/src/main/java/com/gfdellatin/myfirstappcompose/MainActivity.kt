31b1bedb6bb8e8b67961a1e01e1235423189b3af
GuilhermeDellatin <guidellatin@gmail.com>
2021-12-01 21:01:35 -0300

    Adding navController in MainActivity.kt

--- app/src/main/java/com/gfdellatin/myfirstappcompose/MainActivity.kt
+++ app/src/main/java/com/gfdellatin/myfirstappcompose/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.gfdellatin.myfirstappcompose.ui.theme.MyFirstAppComposeTheme
 
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
-    @ExperimentalAnimationApi
+    
+    lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyFirstAppComposeTheme {
-                IntroScreen()
+                
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+                
             }
         }
     }
