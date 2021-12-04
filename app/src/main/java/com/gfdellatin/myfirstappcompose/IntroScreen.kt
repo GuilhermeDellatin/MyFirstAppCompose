ac2afc97238ed8ce1bad67472dee9c39432e59f3
GuilhermeDellatin <guidellatin@gmail.com>
2021-12-04 16:31:09 -0300

    Adding clickable arrow to move PlayListScreen.kt

--- app/src/main/java/com/gfdellatin/myfirstappcompose/IntroScreen.kt
+++ app/src/main/java/com/gfdellatin/myfirstappcompose/IntroScreen.kt
@@ -4,17 +4,18 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,15 +43,31 @@ fun IntroScreen(
                 contentDescription = stringResource(id = R.string.content_logo)
             )
             AnimatedVisibility(expanded) {
-                Text(
-                    modifier = Modifier.clickable {
-                        navController.navigate(route = NavControllerScreen.PlayList.route)
-                    },
-                    text = stringResource(R.string.jetpack_university),
-                    fontSize = 24.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Light
-                )
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Text(
+                        modifier = Modifier.clickable {
+                            navController.navigate(route = NavControllerScreen.PlayList.route)
+                        },
+                        text = stringResource(R.string.jetpack_university),
+                        fontSize = 24.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Light,
+                        textAlign = TextAlign.Justify
+                    )
+                    Image(
+                        modifier = Modifier
+                            .clickable {
+                                navController.navigate(route = NavControllerScreen.PlayList.route)
+                            }
+                            .size(40.dp),
+                        painter = painterResource(id = R.drawable.ic_arrow_forward),
+                        contentDescription = stringResource(id = R.string.content_forward_playlist),
+                        alignment = Alignment.CenterEnd
+                    )
+                }
             }
         }
     }
